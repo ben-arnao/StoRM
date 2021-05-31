3ffc00c753da3eaa3ee12b3116815d5c60a51b94
Ben Arnao <arnaoben@gmail.com>
2021-05-31 14:59:29 -0400

    Update compare_to_optuna.py

--- examples/compare_to_optuna.py
+++ examples/compare_to_optuna.py
@@ -8,11 +8,14 @@ from tensorflow import keras
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import random
+import time
 
 BATCHSIZE = 256
 CLASSES = 10
-EPOCHS = 25
-NUM_TRIALS = 100
+EPOCHS = 30
+TRIALS_PER_TUNE = 60
+NUM_COMPARISONS = 50
 
 # download dataset from https://archive.ics.uci.edu/ml/datasets/wine+quality
 df = pd.read_csv('C:/Users/Ben/Downloads/winequality.csv')
@@ -24,13 +27,7 @@ y = df[target]
 # offset of target breaks sparse
 y -= 3
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-
 num_classes = df[target].nunique()
-
-scaler = StandardScaler()
-x_train = scaler.fit_transform(x_train)
-x_test = scaler.transform(x_test)
 
 storm_scores = []
 optuna_scores = []
@@ -52,12 +49,17 @@ class BalancedSparseCategoricalAccuracy(keras.metrics.SparseCategoricalAccuracy)
         return super().update_state(y_true, y_pred, sample_weight=weight)
 
 
-for x in range(25):
+for n in range(NUM_COMPARISONS):
+    random.seed(time.time())
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=random.randint(0, 100))
+    scaler = StandardScaler()
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.transform(x_test)
 
     def build_model_storm(hp):
-        n_layers = hp.Param('n_layers', [1, 2, 3, 4], ordered=True)
-        num_hidden = hp.Param('num_hidden', [4, 8, 16, 32, 64, 128, 256, 512], ordered=True)
-        weight_decay = hp.Param('weight_decay', [1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3], ordered=True)
+        n_layers = hp.Param('n_layers', [1, 2, 3, 4, 5], ordered=True)
+        weight_decay = hp.Param('weight_decay', [0, 1e-5, 1e-4, 1e-3], ordered=True)
+        num_hidden = hp.Param('num_hidden', [100, 200, 300, 400, 500], ordered=True)
         model = tf.keras.Sequential()
         model.add(tf.keras.layers.Flatten())
         for i in range(n_layers):
@@ -83,11 +85,11 @@ for x in range(25):
         )
 
         opt = hp.Param('optimizer', ['adam', 'sgd'])
-        lr = hp.Param('learning_rate', [1e-2, 1e-3, 1e-4, 1e-5], ordered=True)
+        lr = hp.Param('learning_rate', [1e-2, 1e-3, 1e-4], ordered=True)
         if opt == 'adam':
-            optimizer = Adam(lr=lr, epsilon=hp.Param('epsilon', [1e-14, 1e-12, 1e-10, 1e-8, 1e-6, 1e-4], ordered=True))
+            optimizer = Adam(lr=lr, epsilon=hp.Param('epsilon', [1e-12, 1e-10, 1e-8, 1e-6], ordered=True))
         else:
-            optimizer = SGD(lr=lr, momentum=hp.Param('momentum', [0.8, 0.9, 0.95], ordered=True))
+            optimizer = SGD(lr=lr, momentum=hp.Param('momentum', [0.85, 0.9, 0.95], ordered=True))
 
         loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
         model.compile(loss=loss_fn, optimizer=optimizer, metrics=['accuracy'])
@@ -111,7 +113,7 @@ for x in range(25):
                         objective_direction='max',
                         init_random=10,
                         randomize_axis_factor=0.5,
-                        max_iters=NUM_TRIALS,
+                        max_iters=TRIALS_PER_TUNE,
                         overwrite=True)
         tuner.search()
 
@@ -119,32 +121,29 @@ for x in range(25):
 
 
     def build_model_optuna(trial):
-        n_layers = trial.suggest_int("n_layers", 1, 4)
-        num_hidden = trial.suggest_int("n_units", 4, 512, log=True)
-        weight_decay = trial.suggest_float("weight_decay", 1e-8, 1e-3, log=True)
+        n_units = trial.suggest_int("n_units", 100, 500)
+        n_layers = trial.suggest_int("n_layers", 1, 5)
+        weight_decay = trial.suggest_float("weight_decay", 1e-6, 1e-3, log=True)
         model = tf.keras.Sequential()
         model.add(tf.keras.layers.Flatten())
-
-        batch_norm = trial.suggest_categorical("batch_norm", [True, False])
-        add_noise = trial.suggest_categorical("add_noise", [True, False])
-        dropout = trial.suggest_float("dropout", 0, 0.5)
 
         for i in range(n_layers):
             model.add(
                 tf.keras.layers.Dense(
-                    num_hidden,
+                    n_units,
                     activation=trial.suggest_categorical("activation", ['relu', 'tanh', 'elu', 'selu']),
                     kernel_regularizer=tf.keras.regularizers.l2(weight_decay),
                 )
             )
 
-            if batch_norm:
+            if trial.suggest_categorical("batch_norm", [True, False]):
                 model.add(BatchNormalization())
 
-            if add_noise:
-                model.add(GaussianNoise(trial.suggest_float("adam_learning_rate", 1e-5, 1e-1, log=True)))
+            if trial.suggest_categorical("add_noise", [True, False]):
+                model.add(GaussianNoise(trial.suggest_float("noise_value", 1e-5, 1e-1, log=True)))
 
-            model.add(Dropout(dropout))
+            if trial.suggest_categorical("add_dropout", [True, False]):
+                model.add(Dropout(trial.suggest_float("dropout", 0.05, 0.5)))
 
         model.add(
             tf.keras.layers.Dense(CLASSES, kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
@@ -154,15 +153,15 @@ for x in range(25):
         optimizer_options = ["Adam", "SGD"]
         optimizer_selected = trial.suggest_categorical("optimizer", optimizer_options)
         if optimizer_selected == "Adam":
-            kwargs["learning_rate"] = trial.suggest_float("adam_learning_rate", 1e-5, 1e-2, log=True)
+            kwargs["learning_rate"] = trial.suggest_float("adam_learning_rate", 1e-4, 1e-2, log=True)
             kwargs["epsilon"] = trial.suggest_float(
-                "adam_epsilon", 1e-14, 1e-4, log=True
+                "adam_epsilon", 1e-12, 1e-6, log=True
             )
         elif optimizer_selected == "SGD":
             kwargs["learning_rate"] = trial.suggest_float(
-                "sgd_opt_learning_rate", 1e-5, 1e-2, log=True
+                "sgd_opt_learning_rate", 1e-4, 1e-2, log=True
             )
-            kwargs["momentum"] = trial.suggest_float("sgd_opt_momentum", 0.8, 0.95)
+            kwargs["momentum"] = trial.suggest_float("sgd_opt_momentum", 0.85, 0.95)
 
         optimizer = getattr(tf.optimizers, optimizer_selected)(**kwargs)
 
@@ -181,9 +180,10 @@ for x in range(25):
 
     if __name__ == "__main__":
         study = optuna.create_study(direction="maximize")
-        study.optimize(objective, n_trials=NUM_TRIALS)
+        study.optimize(objective, n_trials=TRIALS_PER_TUNE)
 
         optuna_scores.append(study.best_trial.value)
 
-    print('storm scores mean:', np.mean(storm_scores), 'std:', np.std(storm_scores))
-    print('optuna scores mean:', np.mean(optuna_scores), 'std:', np.std(optuna_scores))
+    print('trial num:', len(storm_scores))
+    print('storm average score:', np.mean(storm_scores))
+    print('optuna average score:', np.mean(optuna_scores))
