eb11826735beda7a46d7c34bf50ad8440fb245c0
Ben Arnao <arnaoben@gmail.com>
2021-08-09 21:35:00 -0400

    Update tuner.py

--- src/storm_tuner/tuner.py
+++ src/storm_tuner/tuner.py
@@ -12,7 +12,7 @@ class Tuner:
     def __init__(self,
                  project_dir=None,
                  build_fn=None,
-                 randomize_axis_factor=0.7,
+                 randomize_axis_factor=0.75,
                  init_random=5,
                  objective_direction='max',
                  overwrite=True,
